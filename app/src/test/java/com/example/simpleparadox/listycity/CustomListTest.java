28d21c345229078278cab04d37f88e801925acbc
Csongor-Szepesvari <csongor.szepesvari@gmail.com>
2021-11-01 11:44:48 -0600

    implemented add city

--- app/src/test/java/com/example/simpleparadox/listycity/CustomListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CustomListTest.java
@@ -1,4 +1,31 @@
+package com.example.simpleparadox.listycity;
+
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CustomListTest {
-  
+    private CustomList list;
+
+    @BeforeEach
+    public void createList(){
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("Halifax", "NS"));
+        assertEquals(list.getCount(), listSize+1);
+    }
+
+
+
+
 }
